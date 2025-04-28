76dd1610fb97b56a661b738abeaa6c0772fd7d1e
shahariarshawon <shahariararafat7739@gmail.com>
2025-04-28 10:31:56 +0600

    added the tailwind and daisyui

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>
+      Auction Gallery Site
+     </h1>
+    </>
+  )
+}
+
+export default App
